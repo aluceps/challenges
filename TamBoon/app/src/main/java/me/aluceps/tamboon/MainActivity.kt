9682e242bffe69adf2d4d354140d3f65f703327e
aluceps <aluceps@gmail.com>
2020-01-14 13:31:37 +0900

    update libraries and support databinding

--- TamBoon/app/src/main/java/me/aluceps/tamboon/MainActivity.kt
+++ TamBoon/app/src/main/java/me/aluceps/tamboon/MainActivity.kt
@@ -1,12 +1,18 @@
 package me.aluceps.tamboon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import me.aluceps.tamboon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding.root
     }
 }
