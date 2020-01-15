dba77730bf83a390cb40599de2a8b76736cf7bd4
aluceps <aluceps@gmail.com>
2020-01-15 18:46:09 +0900

    fix to set post data

--- tamboon-server/server.go
+++ tamboon-server/server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"io/ioutil"
 
 	"github.com/omise/omise-go"
 	"github.com/omise/omise-go/operations"
@@ -40,23 +41,47 @@ func (handler *TamboonHandler) POST(resp http.ResponseWriter, req *http.Request)
 	donation := &Donation{}
 	defer req.Body.Close()
 
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("io error")
+		return
+	}
+
+	jsonBytes := ([]byte)(b)
+	data := new(Donation)
+	if err := json.Unmarshal(jsonBytes, data); err != nil {
+		fmt.Println("JSON Unmarshal error:", err)
+		return
+	}
+
+	donation.Name = data.Name
+	donation.Token = data.Token
+	donation.Amount = data.Amount
+
+	fmt.Println("step:1")
 	if e := json.NewDecoder(req.Body).Decode(donation); e != nil {
+		fmt.Println("step:2")
 		http.Error(resp, e.Error(), 400)
 		return
 	}
 
+	fmt.Println("step:3")
 	charge, operation := &omise.Charge{}, &operations.CreateCharge{
 		Card:        donation.Token,
 		Amount:      donation.Amount,
 		Currency:    "THB",
 		Description: donation.Name,
 	}
+	fmt.Println("step:4")
 	if e := handler.client.Do(charge, operation); e != nil {
+		fmt.Println("step:5")
 		http.Error(resp, e.Error(), 400)
 		return
 	}
 
+	fmt.Println("step:6")
 	if e := json.NewEncoder(resp).Encode(&Result{Success: true}); e != nil {
+		fmt.Println("step:7")
 		http.Error(resp, e.Error(), 500)
 		return
 	}
